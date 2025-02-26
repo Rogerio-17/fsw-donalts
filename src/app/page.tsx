0ddf5d03bd161842b5b4c4261d4f5a488b3c362b
Rogerio-17 <rogeriojmf10@gmail.com>
2025-02-25 21:49:02 -0300

    chore: adiciona nova configuração do prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 export default function HomePage() {
   return (
-    <div>
+    <div className="w-full rounded-xl border border-red-500 p-5">
       <h1>Home Page</h1>
       <Button>teste</Button>
     </div>
